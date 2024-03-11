52cf0572602639091f3300ccfdf52069f162a3a9
Jan Kolipinski <efiflak@gmail.com>
2024-03-11 23:46:47 +0100

    back

--- duckduckgo.py
+++ duckduckgo.py
@@ -18,4 +18,4 @@ def text_for_fish(name : str):
 
 def byry(name : str):
     with open("ryby/byry/"+filename(name), "w") as s:
-        s.write("# "+name+"\n"+text_for_fish(name))
+        s.write("## [back](../index.md) \n" +"# "+name+"\n"+text_for_fish(name))
