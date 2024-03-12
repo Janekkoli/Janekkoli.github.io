d3b78e65538a022194a0711d894ed8b13d3ff34a
Jan Kolipinski <efiflak@gmail.com>
2024-03-12 01:10:00 +0100

    wiki

--- ryby/index.md
+++ ryby/index.md
@@ -1,7 +1,7 @@
 # Strona zaliczeniowa na przedmiot AWWW (Akademicki Wykład Wędkarstwa Wysokogórkiego) na wydziale MIMUW (Matematyka, Informatyka i Mechanika Uniwersytet Wędkarski)
 
 [Atlas ryb](https://rybyswiata.pl/atlas-ryb/#gref)
-- [African glass catfish](byry/African_glass_catfish.md)
-- [African lungfish](byry/African_lungfish.md)
-- [Aholehole](byry/Aholehole.md)
-- [Airbreathing catfish](byry/Airbreathing_catfish.md)
+- [African glass catfish](byry/African_glass_catfish.md) [wiki](https://en.wikipedia.org/wiki/African_glass_catfish)
+- [African lungfish](byry/African_lungfish.md) [wiki](https://en.wikipedia.org/wiki/African_lungfish)
+- [Aholehole](byry/Aholehole.md) [wiki](https://en.wikipedia.org/wiki/Aholehole)
+- [Airbreathing catfish](byry/Airbreathing_catfish.md) [wiki](https://en.wikipedia.org/wiki/Airbreathing_catfish)
--- scrapper.py
+++ scrapper.py
@@ -2,42 +2,41 @@ from bs4 import BeautifulSoup
 import os
 import requests
 from duckduckgo import *
+
+
+
 page_url = 'https://en.wikipedia.org/wiki/List_of_fish_common_names'
 page = requests.get(page_url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
 #print(soup.prettify())
 fish_list = []
-mis=0
+mis = 0
 for i in soup.find_all('a'):
     try:
-        if len(i['title'])>40 or '.' in i['title'] or len(i['title'])<3:
+        if len(i['title']) > 40 or '.' in i['title'] or len(i['title']) < 3:
             continue
-        fish_list.append(i['title'])
+        fish_list.append((i['title'], i['href']))
     except:
-        mis+=1
-#print(fish_list.index('African glass catfish'))
-#print(fish_list.index('Zebra tilapia'))
-#print(fish_list[30:1128])
-fish_list=fish_list[30:1128]
-
-#special
+        mis += 1
+# print(fish_list.index('African glass catfish'))
+# print(fish_list.index('Zebra tilapia'))
+# print(fish_list[30:1128])
+fish_list = fish_list[30:1128]
+# special
 fish_list = fish_list[0:4]
 
-for i in range (len(fish_list)):
-    fish_list[i]=("byry/"+filename(fish_list[i]), fish_list[i])
-    byry(fish_list[i][1])
+for i in range(len(fish_list)):
+    fish_list[i] = ("byry/" + filename(fish_list[i][0]), fish_list[i][0], fish_list[i][1])
 
-
+for i in fish_list:
+    byry(i[1])
 
 with open("ryby/index.md", "w") as f:
     with open("title.md", "r") as r:
-        f.write(r.read() + "\n") 
+        f.write(r.read() + "\n")
     for i in fish_list:
-        f.write('- [' + i[1] + '](' + i[0] + ')' + "\n")
+        f.write('- [' + i[1] + '](' + i[0] + ') '+ '[wiki]'+'(https://en.wikipedia.org' +i[2]+')' + "\n")
 
-
-#print(mis)
-#print(type(list_all_p))
-        
-
+# print(mis)
+# print(type(list_all_p))
